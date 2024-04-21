e1e8a93d4e72ac10720ea4da3b2c3962e0db708f
rugvedmhatre <rugvedmhatre98@gmail.com>
2024-04-20 23:21:18 -0400

    updating validate_subsequence code

--- AlgoExpert/Arrays/validate_subsequence.py
+++ AlgoExpert/Arrays/validate_subsequence.py
@@ -12,6 +12,16 @@ def validate_subsequence(array, sequence):
         arrayIdx += 1
     return sequenceIdx == len(sequence)
 
+# -- Another Method --
+# def validate_subsequence(array, sequence):
+#     sequenceIdx = 0
+#     for value in array:
+#         if sequenceIdx == len(sequence):
+#             break
+#         if value == sequence[sequenceIdx]:
+#             sequenceIdx += 1
+#     return sequenceIdx == len(sequence)
+
 if __name__ == '__main__':
     array_a = [0, 1, 2, 3, 4, 5, 6, 9, -1, 8, 10]
     subsequence = [1, 6, -1, 10]
