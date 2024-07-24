617bd3f99af94bcef3ff1811628e554cc8540d11
rugvedmhatre <rugvedmhatre98@gmail.com>
2024-07-24 11:53:12 -0400

    adding code for leetcode problem 271

--- LeetCode/271-encode_and_decode_strings.py
+++ LeetCode/271-encode_and_decode_strings.py
@@ -2,58 +2,54 @@
 
 https://leetcode.com/problems/encode-and-decode-strings/ (Premium Access Only)
 
-https://www.lintcode.com/problem/659/ (Free Lintcode Link)
+https://neetcode.io/problems/string-encode-and-decode (Free NeetCode Link)
 
-Design an algorithm to encode a list of strings to a string. The encoded string
-is then sent over the network and is decoded back to the original list of 
-strings.
+Design an algorithm to encode a list of strings to a single string. The encoded
+string is then decoded back to the original list of strings.
 
 Please implement encode and decode functions.
 
 Example 1:
-Input: ["lint","code","love","you"]
-Output: ["lint","code","love","you"]
+Input: ["neet","code","love","you"]
+Output:["neet","code","love","you"]
 Explanation:
 One possible encode method is: "lint:;code:;love:;you"
 
 Example 2:
-Input: ["we", "say", ":", "yes"]
-Output: ["we", "say", ":", "yes"]
+Input: ["we","say",":","yes"]
+Output: ["we","say",":","yes"]
 Explanation:
 One possible encode method is: "we:;say:;:::;yes"
 
-"""
+Constraints:
+0 <= strs.length < 100
+0 <= strs[i].length < 200
+strs[i] contains only UTF-8 characters.
 
 """
-@param: strs: a list of strings
-@return: encodes a list of strings to a single string.
-"""
-def encode(strs):
+
+def encode(strs: list[str]) -> str:
     result = ''
     for word in strs:
         result += str(len(word)) + '#' + word
     return result
 
-"""
-@param: str: A string
-@return: decodes a single string to a list of strings
-"""
-def decode(str):
+def decode(s: str) -> list[str]:
     result = []
     i = 0
     
-    while i < len(str):
+    while i < len(s):
         j = i
-        while str[j] != '#':
+        while s[j] != '#':
             j += 1
         
-        length = int(str[i:j])
-        result.append(str[(j+1):(j+1+length)])
+        length = int(s[i:j])
+        result.append(s[(j+1):(j+1+length)])
         i = j + 1 + length
     return result
 
 if __name__ == '__main__':
-    strs = ["lint", "code", "love", "you"]
+    strs = ["neet", "code", "love", "you"]
     print("Input String   :", strs)
     encodedString = encode(strs)
     print("Encoded String :", encodedString)
