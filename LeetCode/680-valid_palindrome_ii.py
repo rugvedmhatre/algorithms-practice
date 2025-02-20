c60ea5cd8881185ee0ed95acdf9b40f6406e9255
rugvedmhatre <rugvedmhatre98@gmail.com>
2025-02-20 16:27:56 -0500

    adding code for leetcode problem 680

---
+++ LeetCode/680-valid_palindrome_ii.py
@@ -0,0 +1,50 @@
+"""
+
+https://leetcode.com/problems/valid-palindrome-ii/
+
+Given a string s, return true if the s can be palindrome after 
+deleting at most one character from it.
+ 
+
+Example 1:
+Input: s = "aba"
+Output: true
+
+Example 2:
+Input: s = "abca"
+Output: true
+Explanation: You could delete the character 'c'.
+
+Example 3:
+Input: s = "abc"
+Output: false
+ 
+
+Constraints:
+- 1 <= s.length <= 10^5
+- s consists of lowercase English letters.
+
+"""
+
+def validPalindrome(s: str) -> bool:
+    l = 0
+    r = len(s) - 1
+
+    while l < r:
+        if s[l] != s[r]:
+            subStrL, subStrR = s[l+1:r+1], s[l:r]
+            return subStrL == subStrL[::-1] or subStrR == subStrR[::-1]
+        l += 1
+        r -= 1
+
+    return True
+
+if __name__ == "__main__":
+    s = "aba"
+    print(validPalindrome(s)) # True
+
+    s = "abca"
+    print(validPalindrome(s)) # True
+
+    s = "abc"
+    print(validPalindrome(s)) # False
