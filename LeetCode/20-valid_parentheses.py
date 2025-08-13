70e3630b119490c8bb8d077e54ffe11f1956ce78
rugvedmhatre <rugvedmhatre98@gmail.com>
2025-08-13 17:15:47 -0400

    updating code for leetcode problem 20

--- LeetCode/20-valid_parentheses.py
+++ LeetCode/20-valid_parentheses.py
@@ -31,6 +31,22 @@ s consists of parentheses only '()[]{}'.
 
 """
 
+# Another Solution
+# def isValid(s: str) -> bool:
+#     stack = []
+#     for c in s:
+#         if c in ('(', '[', '{'):
+#             stack.append(c)
+#         elif len(stack) != 0 and c == ')' and stack[-1] == '(':
+#             stack.pop()
+#         elif len(stack) != 0 and c == ']' and stack[-1] == '[':
+#             stack.pop()
+#         elif len(stack) != 0 and c == '}' and stack[-1] == '{':
+#             stack.pop()
+#         else:
+#             return False
+#     return len(stack) == 0
+
 def isValid(s: str) -> bool:
     brackStack = list()
     brackMap = {')':'(', ']':'[', '}':'{'}
--- amazon_questions.md
+++ amazon_questions.md
@@ -10,6 +10,18 @@ _Easy_
 - [Two Sum](https://github.com/rugvedmhatre/algorithms-practice/blob/main/LeetCode/1-two_sum.py)
 - [Two Sum Less Than K](https://github.com/rugvedmhatre/algorithms-practice/blob/main/LeetCode/1099-two_sum_less_than_k.py)
 
+## Sliding Window
+
+_Easy_
+
+- [Best Time to Buy and Sell Stock](https://github.com/rugvedmhatre/algorithms-practice/blob/main/LeetCode/121-best_time_to_buy_and_sell_stock.py)
+
+## Stack
+
+_Easy_
+
+- [Valid Parentheses](https://github.com/rugvedmhatre/algorithms-practice/blob/main/LeetCode/20-valid_parentheses.py)
+
 ## Linked List
 
 _Easy_
